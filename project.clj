ec1bdfbe1b17a39cb883e57b057aeec47cc8dae9
Savyasachi <jsavyasachi@gmail.com>
2026-06-08 22:12:31 -0700

    build: load clojars credentials from repositories

--- project.clj
+++ project.clj
@@ -2,10 +2,10 @@
   :description "Digest algorithms (MD5, SHA ...) for Clojure"
   :author "Miki Tebeka <miki.tebeka@gmail.com>"
   :url "https://github.com/jsavyasachi/digest"
-  :deploy-repositories [["clojars" {:url "https://repo.clojars.org"
-                                    :username :env/clojars_username
-                                    :password :env/clojars_password
-                                    :sign-releases false}]]
+  :repositories [["clojars" {:url "https://repo.clojars.org"
+                             :username :env/clojars_username
+                             :password :env/clojars_password
+                             :sign-releases false}]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]])
